870b356a00605a8d18ac2835f8f286da81112372
kabir4874 <kabir.cse.bd@gmail.com>
2024-09-19 13:56:25 +0600

    create project

--- dist/src/controllers/projectController.js
+++ dist/src/controllers/projectController.js
@@ -9,16 +9,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProjects = void 0;
-const extension_1 = require("@prisma/client/extension");
-const prisma = new extension_1.PrismaClient();
+exports.createProject = exports.getProjects = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const getProjects = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const projects = yield prisma.project.findMany();
         res.json(projects);
     }
     catch (error) {
-        res.status(500).json({ message: 'Error in retrieving projects' });
+        res.status(500).json({ message: "Error in retrieving projects" });
     }
 });
 exports.getProjects = getProjects;
+const createProject = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, description, startDate, endDate } = req.body;
+    try {
+        const newProject = yield prisma.project.create({
+            data: {
+                name,
+                description,
+                startDate,
+                endDate,
+            },
+        });
+        res.status(201).json(newProject);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error in creating projects" });
+    }
+});
+exports.createProject = createProject;
